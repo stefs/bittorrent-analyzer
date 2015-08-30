ea3842e837f4d98472e0ebbd7622a091e20ec68c
Stefan Schindler <stefan@kaloix.de>
2015-08-30 14:09:33 +0200

    combine vm databases

---
+++ evaluation/combine.sql
@@ -0,0 +1,42 @@
+-- docs: https://www.sqlite.org/cli.html
+-- stop after hitting an error
+.bail on
+.print '.bail on'
+
+-- usage: cat combine.sql | sqlite3 | sqlite3 combined.sqlite
+ATTACH DATABASE 'x/vm1.sqlite' AS vm1;
+ATTACH DATABASE 'x/vm1.sqlite' AS vm2;
+
+-- recreate tables
+SELECT sql || ';'
+	FROM vm1.sqlite_master
+	WHERE type='table';
+
+-- table torrent
+.mode insert torrent
+SELECT id, announce_url, info_hash, info_hash_hex, pieces_count, piece_size, complete_threshold, filepath, display_name, gigabyte
+	FROM vm1.torrent;
+SELECT id+50, announce_url, info_hash, info_hash_hex, pieces_count, piece_size, complete_threshold, filepath, display_name, gigabyte
+	FROM vm2.torrent;
+
+-- table peer
+.mode insert peer
+SELECT NULL, host, client, continent, country, latitude, longitude, first_pieces, last_pieces, first_seen, last_seen, max_speed, visits, source, torrent
+	FROM vm1.peer;
+SELECT NULL, host, client, continent, country, latitude, longitude, first_pieces, last_pieces, first_seen, last_seen, max_speed, visits, source, torrent+50
+	FROM vm2.peer;
+
+-- table request
+.mode insert request
+SELECT NULL, timestamp, source, received_peers, duplicate_peers, seeders, completed, leechers, duration_sec, torrent
+	FROM vm1.request;
+SELECT NULL, timestamp, source, received_peers, duplicate_peers, seeders, completed, leechers, duration_sec, torrent+50
+	FROM vm2.request;
+
+-- table statistic
+.mode insert statistic
+ALTER TABLE statistic ADD COLUMN vm INTEGER;
+SELECT NULL, timestamp, peer_queue, visited_queue, unique_incoming, success_active, thread_workload, load_average, memory_mb, server_threads, evaluator_threads, 1
+	FROM vm1.statistic;
+SELECT NULL, timestamp, peer_queue, visited_queue, unique_incoming, success_active, thread_workload, load_average, memory_mb, server_threads, evaluator_threads, 2
+	FROM vm2.statistic;
--- result/README.md
+++ result/README.md
@@ -4,4 +4,4 @@ Usage | Filename
 Timeout parameter | 2015-08-14_17-46-44_faui1-246*
 Hostname collection | 2015-08-26_11-24-21_faui1-246*
 Test evaluation | 2015-08-27_21-03-31_faui1-246*
-Main evaluation
+Main evaluation | TBD
